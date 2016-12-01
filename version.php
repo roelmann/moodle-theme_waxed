5ac614152ac9c01c1084ec23133455bd0f37f417
roglos <roelmann@glos.ac.uk>
2016-12-01 14:57:26 +0000

    Minor style updates/fixes

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016113000;
-$plugin->release  = 'v3.2.0.0';
+$plugin->version   = 2016113001;
+$plugin->release  = 'v3.2.0.0.a';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2016070700;
 $plugin->component = 'theme_waxed';
