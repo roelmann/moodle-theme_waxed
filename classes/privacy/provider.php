5cd7652fcf7a509b0820bad4dbf2362c1f434477
roelmann <oelmann.richard@gmail.com>
2018-05-12 23:31:07 +0100

    Add licence to privacy api

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -22,14 +22,13 @@
  * @copyright  theme_boost - MoodleHQ
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-// …
- 
+
 namespace theme_waxed\privacy;
- 
-class provider implements 
+
+class provider implements
     // This plugin does not store any personal user data.
     \core_privacy\local\metadata\null_provider {
- 
+
     /**
      * Get the language string identifier with the component's language
      * file to explain why this plugin stores no data.
