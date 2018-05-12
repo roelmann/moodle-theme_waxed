f2558651b492ac935ef5bed3d9c3f2cd9750bab1
roelmann <oelmann.richard@gmail.com>
2018-05-12 23:20:59 +0100

    Add licence to privacy api

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -1,4 +1,21 @@
-<?php
+<?php // This file is part of Moodle - http://moodle.org/ // // Moodle is free
+software: you can redistribute it and/or modify // it under the terms of the GNU
+General Public License as published by // the Free Software Foundation, either
+version 3 of the License, or // (at your option) any later version. // // Moodle
+is distributed in the hope that it will be useful, // but WITHOUT ANY WARRANTY;
+without even the implied warranty of // MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the // GNU General Public License for more details. //
+// You should have received a copy of the GNU General Public License // along
+with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file.
+ *
+ * @package    theme_waxed
+ * @copyright  2018 Richard Oelmann
+ * @copyright  theme_boost - MoodleHQ
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 // …
  
 namespace theme_waxed\privacy;
